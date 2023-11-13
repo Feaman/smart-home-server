6e03d60f8426cea5fc8bdcbd01c39396f7654e93
Pavel Aksyonov <elf.feaman@gmail.com>
2023-11-13 11:59:16 +0300

    Commit

--- src/api.ts
+++ src/api.ts
@@ -24,7 +24,7 @@ server.listen(PORT, async function () {
   console.log(`Application server started on port ${PORT}`)
 })
 
-function httpRequest(params: RequestOptions): Promise<{ entity_id: string, state: string }[]> {
+function httpRequest(params: RequestOptions, data?: { entity_id: string }): Promise<{ entity_id: string, state: string }[]> {
   return new Promise(function (resolve, reject) {
     const request = http.request(params, function (response) {
       if (Number(response.statusCode) < 200 || Number(response.statusCode) >= 300) {
@@ -48,6 +48,10 @@ function httpRequest(params: RequestOptions): Promise<{ entity_id: string, state
     request.on('error', function (err) {
       reject(err)
     })
+    
+    if (data) {
+      request.write(JSON.stringify(data))
+    }
 
     request.end()
   })
@@ -64,11 +68,22 @@ app.get(
         'sensor.temperature_small_room',
         'sensor.temperature_big_room',
         'sensor.temperature_hall',
+
         'sensor.humidity_street',
         'sensor.humidity_balcony',
         'sensor.humidity_small_room',
         'sensor.humidity_big_room',
         'sensor.humidity_hall',
+
+        'input_boolean.night_mode',
+        'input_boolean.hallway_persist_mode',
+        'input_boolean.bathroom_bathing_mode',
+        'input_boolean.big_room_night_mode',
+
+        'switch.wall_switch_small_room_light',
+        'light.light_small_room_night_light',
+        'switch.wall_switch_hall_light',
+        'switch.wall_switch_kitchen_light_left',
       ]
       const result = data
         .filter( entity => allowedEntities.includes(entity.entity_id))
@@ -80,3 +95,16 @@ app.get(
     }
   },
 )
+
+app.post('/toggle/:domain', async (request, response, next) => {
+  try {
+    const data = await httpRequest(
+      { ...baseOptions, path: `/api/services/${request.params.domain}/toggle`, method: 'POST' },
+      { entity_id: request.body.entityId }
+    )
+    return response.status(200).json(data)
+  }
+  catch (error) {
+    return next(error)
+  }
+})
